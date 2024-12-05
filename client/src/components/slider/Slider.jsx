cd42b12830ba3d8e8964539347e70d6047b750f2
Sandesh Dalvi <dalviss10@gmail.com>
2024-12-05 12:11:49 +0530

    single page slider

---
+++ client/src/components/slider/Slider.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import "./slider.scss";
+
+const Slider = ({ images }) => {
+  const [imageIndex, setImageIndex] = useState(null);
+
+  const changeSlides = (direction) => {
+    if (direction === "left") {
+      if (imageIndex === 0) {
+        setImageIndex(images.length - 1);
+      } else {
+        setImageIndex(imageIndex - 1);
+      }
+    }
+
+    if (direction === "right") {
+      if (imageIndex === images.length - 1) {
+        setImageIndex(0);
+      } else {
+        setImageIndex(imageIndex + 1);
+      }
+    }
+  };
+
+  return (
+    <div className="slider">
+      {imageIndex !== null && (
+        <div className="fullSlider">
+          <div className="arrow" onClick={() => changeSlides("left")}>
+            <img src="/arrow.png" alt="" />
+          </div>
+          <div className="imgContainer">
+            <img src={images[imageIndex]} alt="" />
+          </div>
+          <div className="arrow" onClick={() => changeSlides("right")}>
+            <img src="/arrow.png" className="right" alt="" />
+          </div>
+          <div className="close" onClick={() => setImageIndex(null)}>
+            X
+          </div>
+        </div>
+      )}
+
+      <div className="bigImage">
+        <img src={images[0]} alt="" onClick={() => setImageIndex(0)} />
+      </div>
+      <div className="smallImages">
+        {images.slice(1).map((image, index) => (
+          <img
+            src={image}
+            key={index}
+            alt=""
+            onClick={() => setImageIndex(index + 1)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
--- client/src/page/listPage/ListPage.jsx
+++ client/src/page/listPage/ListPage.jsx
@@ -1,5 +1,6 @@
 import Card from "../../components/card/Card";
 import Filter from "../../components/filter/Filter";
+import Map from "../../components/map/Map";
 import { listData } from "../../lib/dummyData";
 
 import "./listPage.scss";
@@ -17,7 +18,9 @@ const ListPage = () => {
           ))}
         </div>
       </div>
-      <div className="mapContainer"></div>
+      <div className="mapContainer">
+        <Map items={data} />
+      </div>
     </div>
   );
 };
